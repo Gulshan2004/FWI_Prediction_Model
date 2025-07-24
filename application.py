7a69ebd9acb32951e7bd471d1b692c230f0cdb26
Gulshan2004 <gulshankumar1923070@gmail.com>
2025-07-24 18:50:23 +0530

    Updated Flask app and HTML for deployment

--- application.py
+++ application.py
@@ -33,7 +33,7 @@ def predict_datapoint():
         Classes=float(request.form.get('Classes'))
         Region=float(request.form.get('Region'))
 
-        new_data_scaled=standard_scaler.transform([[Tempreature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
+        new_data_scaled=standard_scaler.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
         result=ridge_model.predict(new_data_scaled)
 
         return render_template('home.html',results=result[0])
