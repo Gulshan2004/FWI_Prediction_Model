5ad3c7c7068cca8849824026ce30019e2b0660a8
Gulshan2004 <gulshankumar1923070@gmail.com>
2025-07-24 18:07:54 +0530

    Fix route conflicts and render home.hmtl

--- application.py
+++ application.py
@@ -41,9 +41,6 @@ def predict_datapoint():
 
     else:
         return render_template('home.html')
-
-
-def index():
-    return render_template('index.html')
+    
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
